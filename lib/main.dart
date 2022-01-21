336912027d84108c24373b851e7d11b37d5e15c6
abdraevaeleonora <abdraevaeleonora@gmail.com>
2022-01-22 00:03:53 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key, @required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String studentName = "Jon";
+  int studentGrade = 5;
+
+  List<String> okuuchular=[
+  'Jone',//0
+  "Jane",//1
+  "Jake"//2
+  studentName, //3
+ ];
+
+log(murun  okuuchular->1==>>${okuuchular2}[0]}')'
+
+studentName='Manas';
+
+
+
+
+
+List<int> baalar =[
+3,
+4,
+5
+studentGrade
+];
+
+log('kiyin okuuchular2==>> $okuuchular2');
+
+Log('baalar ==>> $baalar');
+}
+
+
+
+
+
+
+
+
+
+
+  ///
+  
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
